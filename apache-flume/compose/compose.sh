ffefe5e123ebb4e6a0caa8e2d1ea45219730b805
ilpan <pna.dev@outlook.com>
2018-03-17 22:48:50 +0800

    add and edit: not finished for flume tier2

---
+++ apache-flume/compose/compose.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+print_usage() {
+    echo "Run Flume cluster on docker use docker-compose"
+    echo ""
+    echo "Usage: $0 [init|start|stop|remove]"
+    echo ""
+    echo "    -i|init   : Init and start flume cluster"
+    echo "    -s|start  : Start flume cluster"
+    echo "    -S|stop   : Stop flumme cluster"
+    echo "    -r|remove : Remove flume cluster and related settings"
+}
+
+if [ $# -ne 1 ]; then
+    print_usage
+    exit 1
+fi
+
+
+tier1=("tier1-1" "tier1-2" "tier1-3" "tier1-4")
+tier2=("tier2-1" "tier2-2")
+
+conf-dir="/usr/local/.jenv/candidates/flume/current/conf/"
+jenv-init="/usr/local/.jenv/bin/jenv-init.sh"
+
+# 创建并启动容器
+function create_cluster() {
+    docker-compose up -d
+}
+
+function start_daemon() {
+    for agent in ${tier2[@]}
+    do
+        docker exec -itd flume-${agent} bash -c "source ${jenv-init} && flume-ng agent -n a1 -c ${conf-dir} -f ${agent}.properties &"
+    done
+    
+    for agent in ${tier1[@]}
+    do
+        docker exec -itd flume-${agent} bash -c "source ${jenv-init} && flume-ng agent -n a1 -c ${conf-dir} -f ${agent}.properties &"
+    done
+}
+
+function init_cluster() {
+    create_cluster
+    start_daemon
+}
+
+
+function start_cluster() {
+    docker-compose start
+    start_daemon
+}
+
+
+function stop_cluster() {
+    docker-compose stop
+}
+
+
+function remove_cluster() {
+    docker-compose down
+}
+
+
+case "$1" in
+    "-i"|"init")
+        init_cluster
+        echo "### flume 集群成功初始化并启动 ###"
+        ;;
+    "-s"|"start")
+        start_cluster
+        echo "### 成功启动 flume 集群 ###"
+        ;;
+    "-S"|"stop")
+        stop_cluster
+        echo "### 成功stop flume 集群 ###"
+        ;;
+    "-r"|"remove")
+        remove_cluster
+        echo "### 集群移除成功 ###"
+        ;;
+    "-h"|"help")
+        print_usage
+        exit 0
+        ;;
+    *)
+        print_usage
+        exit 1
+        ;;
+esac
--- apache-hbase/compose/compose.sh
+++ apache-hbase/compose/compose.sh
@@ -5,10 +5,10 @@ print_usage() {
     echo ""
     echo "Usage: $0 [init|start|stop|remove]"
     echo ""
-    echo "    -i|init   : Init and start hhbase cluster"
+    echo "    -i|init   : Init and start hbase cluster"
     echo "    -s|start  : Start hbase cluster"
     echo "    -S|stop   : Stop hbase cluster"
-    echo "    -r|remove : Remove hbase cluseter and related settings"
+    echo "    -r|remove : Remove hbase cluster and related settings"
 }
 
 if [ $# -ne 1 ]; then
@@ -17,49 +17,65 @@ if [ $# -ne 1 ]; then
 fi
 
 
+hbases=("hbase-master" "hbase-regionserver1" "hbase-regionserver2" "hbase-regionserver3")
+
 # 创建并启动容器
 function create_cluster() {
     docker-compose up -d hmaster 
 }
 
-hbases=("hbase-master" "hbase-regionserver1" "hbase-regionserver2" "hbase-regionserver3")
-zks=${hbases:1}
-# 启动 Zookeeper 集群
-function start_zk_cluster() {
-    for zk in ${zks[@]}
-    do
-        docker exec -it ${zk} bash -c "source /home/hdp/.jenv/bin/jenv-init.sh && zkServer.sh start"
-    done
-}
 
 # 配置容器间免密登录
 function login_without_passwd() {
     for hbase in ${hbases[@]}
     do
-        docker exec -it ${hbase} bash -c "ssh-keygen -t rsa -P '' &&  chmod 700 /home/hdp/.ssh && chmod 600 /home/hdp/.ssh/id_rsa && cat /home/hdp/.ssh/id_rsa.pub >> /home/hdp/.ssh/authorized_keys && chmod 644 /home/hdp/.ssh/authorized_keys"
+        docker exec -it ${hbase} bash -c "sudo mkdir -p /data/ && sudo chown -R hdp:hadoop /data/"
+        docker exec -it ${hbase} bash -c "sudo sed -i 's/.*StrictHostKeyChecking ask/StrictHostKeyChecking no/' /etc/ssh/ssh_config"
+        docker exec -it ${hbase} bash -c "ssh-keygen &&  chmod 700 /home/hdp/.ssh && chmod 600 /home/hdp/.ssh/id_rsa && cat /home/hdp/.ssh/id_rsa.pub >> /home/hdp/.ssh/authorized_keys && chmod 644 /home/hdp/.ssh/authorized_keys"
     done
     # hadoop-master & hadoop-slave* 是主机名，为了与 ilpan/apache-hadoop中的配置一致 (虽然也可以修改)
     docker exec -it hbase-master bash -c "ssh-copy-id hadoop-slave1 && ssh-copy-id hadoop-slave2 && ssh-copy-id hadoop-slave3"
     docker exec -it hbase-regionserver1 bash -c "ssh-copy-id hadoop-master && ssh-copy-id hadoop-slave2 && ssh-copy-id hadoop-slave3"
     docker exec -it hbase-regionserver2 bash -c "ssh-copy-id hadoop-master && ssh-copy-id hadoop-slave1 && ssh-copy-id hadoop-slave3"
-    docker exec -it hbase-regionserver3 bash -c "ssh-copy-id hadoop-master && ssh-copy-id hadoop-master && ssh-copy-id hadoop-slave2"
+    docker exec -it hbase-regionserver3 bash -c "ssh-copy-id hadoop-master && ssh-copy-id hadoop-slave1 && ssh-copy-id hadoop-slave2"
+}
+
+# cd 后面的指令还是在当前目录下执行的
+# 手动进入zookeeper目录启动
+function start_zk_cluster() {
+#    # 直接通过判断是否存在 zk network来判断是否存在zk cluster
+#    cd ../../apache-hbase/compose/
+#    docker network ls | grep 'compose_zookeeper-network'
+#    if [ $? -ne 0 ]; then
+#        ./compose.sh -i
+#    else
+#        ./compose.sh -S
+#        ./compose.sh -s
+#    fi
+#    # 回到原来的目录
+#    cd ../../apache-hbase/compose
+    echo "请进入apache-zookeeper/compose目录使用./compose.sh [-i|-s](初始化|启动) zk 集群"
 }
 
 function start_daemon() {
-    docker exec -it hbase-master bash -c "source /home/hdp/.jenv/bin/jenv-init.sh && jenv cd hadoop && sbin/start-all.sh && sbin/hadoop-daemon.sh start zkfc && start-hbase.sh && jps"
+    # 1) 启动 hadoop 集群
+    docker exec -it hbase-master bash -c "source /home/hdp/.jenv/bin/jenv-init.sh && jenv cd hadoop && sbin/start-all.sh && jps"
+    # 2) 启动 zookeeper 集群
+    start_zk_cluster
+    # 3) 启动hbase
+    docker exec -it hbase-master bash -c "source /home/hdp/.jenv/bin/jenv-init.sh && start-hbase.sh && jps"
     # 启动 backup master
-    docker exec -it hbase-regionserver1 bash -c "source /home/hdp/.jenv/bin/jenv-init.sh && hbase-daemon.sh start master && jps"
+    # docker exec -it hbase-regionserver1 bash -c "source /home/hdp/.jenv/bin/jenv-init.sh && hbase-daemon.sh start master && jps"
 }
 
 # 初始化
 function init() {
-    docker exec -it hbase-master bash -c "source /home/hdp/.jenv/bin/jenv-init.sh && hdfs zkfc -formatZK && hadoop namenode -format"
+    docker exec -it hbase-master bash -c "source /home/hdp/.jenv/bin/jenv-init.sh && hadoop namenode -format"
     start_daemon
 }
 
 function init_cluster() {
     create_cluster
-    start_zk_cluster
     login_without_passwd
     init
 }
@@ -67,15 +83,25 @@ function init_cluster() {
 
 function start_cluster() {
     docker-compose start
-    start_zk_cluster
     start_daemon
 }
 
+function stop_zk_cluster() {
+#    cd ../../apache-zookeeper/compose/
+#    ./compose.sh -S
+#    cd ../../apache-hbase/compose/
+    echo "请进入 apache-zookeeper/compose 使用 ./compose -s 停止zk集群"
+}
 
 function stop_daemon() {
     # 关闭 backup master
-    docker exec -it hbase-regionserver1 bash -c "source /home/hdp/.jenv/bin/jenv-init.sh && hbase-daemon.sh stop master"
-    docker exec -it hbase-master bash -c "source /home/hdp/.jenv/bin/jenv-init.sh && stop-hbase.sh && jenv cd hadoop && sbin/hadoop-daemon.sh stop zkfc && sbin/stop-all.sh && zkServer.sh stop"
+    # docker exec -it hbase-regionserver1 bash -c "source /home/hdp/.jenv/bin/jenv-init.sh && hbase-daemon.sh stop master"
+    # 1）关闭 hbase
+    docker exec -it hbase-master bash -c "source /usr/local/.jenv/bin/jenv-init.sh && stop-hbase.sh"
+    # 2) 关闭 ZK cluster
+    stop_zk_cluster
+    # 3）关闭 hadoop 集群
+    docker exec -it hbase-master bash -c "source /home/hdp/.jenv/bin/jenv-init.sh && stop-hbase.sh && jenv cd hadoop && sbin/stop-all.sh"
 }
 
 function stop_cluster() {
@@ -83,7 +109,7 @@ function stop_cluster() {
     docker-compose stop
 }
 
-
+# 只删除 hbase 集群，不影响 zookeeper 集群
 function remove_cluster() {
     docker-compose down
 }
