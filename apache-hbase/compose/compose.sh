d6f123f75819a912de44f9e3a7e804cd78b438c1
ilpan <pna.dev@outlook.com>
2018-03-24 00:50:19 +0800

    edit for availability

--- apache-hbase/compose/compose.sh
+++ apache-hbase/compose/compose.sh
@@ -24,29 +24,14 @@ function create_cluster() {
     docker-compose up -d hmaster 
 }
 
-# 手动进入zookeeper目录启动
-function start_zk_cluster() {
-#    # 直接通过判断是否存在 zk network来判断是否存在zk cluster
-#    cd ../../apache-hbase/compose/
-#    docker network ls | grep 'compose_zookeeper-network'
-#    if [ $? -ne 0 ]; then
-#        ./compose.sh -i
-#    else
-#        ./compose.sh -S
-#        ./compose.sh -s
-#    fi
-#    # 回到原来的目录
-#    cd ../../apache-hbase/compose
-    echo "请进入apache-zookeeper/compose目录使用./compose.sh [-i|-s](初始化|启动) zk 集群"
-}
-
 function start_daemon() {
     # 1) 启动 hadoop 集群
     docker exec -it hbase-master bash -c "source ~/.jenv/bin/jenv-init.sh && jenv cd hadoop && sbin/start-all.sh && jps"
-    # 2) 启动 zookeeper 集群
-    start_zk_cluster
-    # 3) 启动hbase
-    docker exec -it hbase-master bash -c "source ~/.jenv/bin/jenv-init.sh && start-hbase.sh && jps"
+
+    echo "Starting HBase ......"
+
+    # 2) 启动hbase, 最后的bash用于阻塞防止master自动关闭, 也便于hbase shell操作
+    docker exec -it hbase-master bash -c "source ~/.jenv/bin/jenv-init.sh && start-hbase.sh && jps && bash"
     # 启动 backup master
     # docker exec -it hbase-regionserver1 bash -c "source /home/hdp/.jenv/bin/jenv-init.sh && hbase-daemon.sh start master && jps"
 }
@@ -68,21 +53,14 @@ function start_cluster() {
     start_daemon
 }
 
-function stop_zk_cluster() {
-#    cd ../../apache-zookeeper/compose/
-#    ./compose.sh -S
-#    cd ../../apache-hbase/compose/
-    echo "请进入 apache-zookeeper/compose 使用 ./compose -s 停止zk集群"
-}
 
 function stop_daemon() {
     # 关闭 backup master
     # docker exec -it hbase-regionserver1 bash -c "source /home/hdp/.jenv/bin/jenv-init.sh && hbase-daemon.sh stop master"
     # 1）关闭 hbase
     docker exec -it hbase-master bash -c "source /usr/local/.jenv/bin/jenv-init.sh && stop-hbase.sh"
-    # 2) 关闭 ZK cluster
-    stop_zk_cluster
-    # 3）关闭 hadoop 集群
+
+    # 2）关闭 hadoop 集群
     docker exec -it hbase-master bash -c "source ~/.jenv/bin/jenv-init.sh && stop-hbase.sh && jenv cd hadoop && sbin/stop-all.sh"
 }
 
